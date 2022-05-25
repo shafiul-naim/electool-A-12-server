b7716de778f0e361905555d0929d23418ced232a
shafiul-naim <shafiul.naim@gmail.com>
2022-05-25 20:03:13 +0600

    jwt token added

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
           };
           const result = await userCollection.updateOne(filter, updateDoc, options); 
           const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-          res.send(result)
+          res.send({result, token})
     });
 
     
