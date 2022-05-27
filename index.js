2ad53e7a267b9797c55390be08e7d59be0c9c30d
shafiul-naim <shafiul.naim@gmail.com>
2022-05-27 22:07:41 +0600

    payment card

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -40,6 +41,27 @@ async function run() {
     const toolsCollection = client.db("electool").collection("tools");
     const orderCollection = client.db("electool").collection("order");
     const userCollection = client.db("electool").collection("user");
+
+
+
+
+
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+        const order = req.body;
+        console.log(order)
+        const price = order.totalPrice;
+        const amount = (price*100);
+        console.log(amount)
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount : amount,
+          currency: 'usd',
+          payment_method_types:['card']
+        });
+        res.send({clientSecret: paymentIntent.client_secret})
+      });
+
+
+
 
     app.get("/tools", verifyJWT, async (req, res) => {
       const query = {};
